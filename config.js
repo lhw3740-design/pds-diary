d8f8c412b383376451f9b560f4d6865892fb7b72
lhw3740-design <lhw3740@gmail.com>
2026-09-16 14:43:45 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Copy your Supabase Project URL and public publishable/anon key here.
 // DO NOT put a service_role/secret key here.
-window.SUPABASE_URL = "https://phsneehzgvnixmopjwnt.supabase.co/rest/v1/";
+window.SUPABASE_URL = "https://phsneehzgvnixmopjwnt.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoc25lZWh6Z3ZuaXhtb3Bqd250Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1MjEzNjksImV4cCI6MjEwNTA5NzM2OX0.k4cj7p5LzKGbjBnuCUfUH28K5Mqb1D_qavLZ1g6h1YQ";
